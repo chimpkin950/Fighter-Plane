30baf8256b2c9858d01f53604ddee21dc9ae2e00
TinaChan04 <jmissj11@gmail.com>
2025-11-09 15:57:50 -0500

    Revert "Files"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,11 @@ public class GameManager : MonoBehaviour
 
     public GameObject enemyOnePrefab;
     public GameObject Enemy2Prefab;
-    public GameObject Enemy3Prefab;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1, 2);
         InvokeRepeating("CreateEnemy2", 4, 6);
-        InvokeRepeating("CreateEnemy3", 2, 4);
     }
 
     // Update is called once per frame
@@ -29,9 +27,5 @@ public class GameManager : MonoBehaviour
    void CreateEnemy2()
     {
         Instantiate(Enemy2Prefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
-    }
-     void CreateEnemy3()
-    {
-        Instantiate(Enemy3Prefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
     }
 }
