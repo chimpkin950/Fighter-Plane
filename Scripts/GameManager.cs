93e872e6b65a0769855b57a7198699c8727dd871
TinaChan04 <jmissj11@gmail.com>
2025-11-15 04:03:27 -0500

    Updated Assets without the Text part..
    File was too big to be one file....

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float verticalScreenSize = 5f;
+    public float horizontalScreenSize = 6f;
+    public int score;
+    public GameObject enemyOnePrefab;
+    public GameObject Enemy2Prefab;
+    public GameObject Enemy3Prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateEnemyOne", 1, 2);
+        score = 0;
+
+        InvokeRepeating("CreateEnemy2", 4, 6);
+        InvokeRepeating("CreateEnemy3", 2, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void CreateEnemyOne()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+   void CreateEnemy2()
+    {
+        Instantiate(Enemy2Prefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+    void CreateEnemy3()
+    {
+        Instantiate(Enemy3Prefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+
+    public void AddScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        //score = score + pointsToAdd;
+        //
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //how to define a variable
+    //1. access modifier: public or private
+    //2. data type: int, float, bool, string
+    //3. variable name: camelCase
+    //4. value: optional
+    public int lives;
+    private float playerSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private float horizontalScreenLimit = 9.5f;
+    private float verticalScreenLimit = 6.5f;
+
+    public GameObject bulletPrefab;
+
+    public GameObject BigBulletPrefab;
+
+    void Start()
+    {
+        playerSpeed = 6f;
+        lives = 3;
+        //This function is called at the start of the game
+
+    }
+    public void LoseALife()
+    {
+        //lives --
+        //reduce the player's life by 1
+        lives--; 
+        if(lives == 0)
+        {
+          
+        }
+    }
+
+    void Update()
+    {
+        //This function is called every frame; 60 frames/second
+        Movement();
+        Shooting();
+
+    }
+
+    void Shooting()
+    {
+        //if the player presses the SPACE key, create a projectile
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        }
+        //if the player presses the E key, create a Big Bullet
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(BigBulletPrefab, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+        }
+    }
+
+    void Movement()
+    {
+        //Read the input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //Move the player
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
+        //Player leaves the screen horizontally
+        if(transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+        //Player leaves the screen vertically
+        if(transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
+        }
+        //Player limited bottom of screen
+        if(transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+
+        if(transform.position.y < -verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, -verticalScreenLimit, 0);
+        }
+    
+    }
+
+
+
+}
